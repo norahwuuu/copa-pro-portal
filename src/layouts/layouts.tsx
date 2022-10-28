3f3cc8a3f3c7a2d3fc8ff8a92707afc34e88f9dd
haonanu <haonan.wu@ulabsystems.com>
2022-10-28 14:51:08 +0800

    fix(layouts.tsx): set children type

--- src/layouts/layouts.tsx
+++ src/layouts/layouts.tsx
@@ -1,6 +1,7 @@
 import theme from "@/theme/theme";
 import { ThemeProvider } from "@mui/material";
+import { ReactChildren } from "react";
 
-export default function Layout({ children }: { children: any }) {
+export default function Layout({ children }: { children: ReactChildren }) {
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 }
