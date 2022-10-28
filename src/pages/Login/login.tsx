33423c17a94dd141fcbde47a11a9ed3ad6088471
Jacky <houxue.liu@ulabsystems.com>
2022-10-28 15:09:14 +0800

    feat: 隐藏login error

--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -43,7 +43,7 @@ const Login: FC<any> = () => {
         mainTitle={loginText.mainTitle}
         subtitle={loginText.subTitle}
         headerInfo={{
-          show: true,
+          show: showLoginError,
           type: "error",
           info: "Username and password combination do not match our records.",
         }}
