b7a3b8c845c887d0949fc63ead7284f94273636c
haonanu <haonan.wu@ulabsystems.com>
2022-11-11 13:53:03 +0800

    fix(footer.less,layouts.tsx): style about ?

--- src/layouts/layouts.tsx
+++ src/layouts/layouts.tsx
@@ -1,12 +1,12 @@
 import Footer from "@/components/Footer/footer";
 import theme from "@/theme/theme";
-import { ThemeProvider } from "@mui/material";
+import { Box, ThemeProvider } from "@mui/material";
 import { ReactChildren } from "react";
 
 export default function Layout({ children }: { children: ReactChildren }) {
   return (
     <ThemeProvider theme={theme}>
-      {children}
+      <Box>{children}</Box>
       <Footer />
     </ThemeProvider>
   );
