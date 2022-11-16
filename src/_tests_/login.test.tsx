9833a77380562a95cb0500cc99d1c21cc863975a
Jacky <houxue.liu@ulabsystems.com>
2022-11-16 17:42:31 +0800

    feat: write login-test by GM code

--- src/_tests_/login.test.tsx
+++ src/_tests_/login.test.tsx
@@ -1,55 +1,93 @@
 // // import { queryLogin } from "@/services/login";
 // import AppRoutes from "../../../../routes"
-// import Login from "../pages/LoginBox/Login/login";
-import useDispatch from "react-redux";
+
 jest.mock("react-redux");
 // const mockFunction = jest.fn()
 
-describe("<Login />", () => {
-  // test("initial load", async () => {
-  //   render(<Login />);
-  //   // expect(screen.getAllByTestId("username")).toBeInTheDocument();
-  //   // expect(screen.getAllByTestId("password")).toBeInTheDocument();
-  // });
-  // test("Should dispaly error when field is empty", async () => {
-  //   const { user } = renderWithRouter(<Login />)
-  //   await act(() => {
-  //     user.click(screen.getByText(/btnLogin/))
-  //   })
-  //   expect(screen.getAllByTestId('username')).toBeInTheDocument()
-  //   expect(screen.getAllByTestId('password')).toBeInTheDocument()
-  // })
-  // test("Should dispaly error when email is invalid format", async () => {
-  //   const { user } = renderWithRouter(<Login />)
-  //   await act(() => {
-  //     user.change(screen.getByLabelText("formfields.email"), { target: { value: "test" } })
-  //     user.change(screen.getByLabelText("formfields.password"), { target: { value: "test" } })
-  //     user.click(screen.getByText(/button.login/))
-  //   })
-  //   expect(screen.getByText(/formfieldErrors.usernameinvalid/)).toBeInTheDocument()
-  //   expect(screen.queryByText(/formfieldErrors.passwordrequired/)).toBeNull()
-  // })
-  test("Should able to submit form with valid username and password", async () => {
-    // useShallowEqualSelector.mockReturnValueOnce(0); // if we have only one selector
-    expect(useDispatch).toHaveBeenCalledTimes(1);
-    expect(useDispatch).toHaveBeenCalledWith({
-      type: "loginSpace/login",
-      payload: {
-        username: "admin@ulabsystems.net",
-        password: "Qwer12#",
+import { act, cleanup, screen } from "@testing-library/react";
+
+import { renderWithRouter } from "@/utils/test.util";
+import AppRoutes from "../../../../routes";
+
+import Login from "../pages/LoginBox/Login/login";
+
+const mockFunction = jest.fn();
+jest.mock("@okta/okta-react", () => {
+  return {
+    useOktaAuth: () => ({
+      authState: { isAuthenticated: true },
+      authService: { handleAuthentication: mockFunction },
+      oktaAuth: {
+        getUser: () =>
+          new Promise((resolve) => {
+            resolve("foo");
+          }),
+        signInWithCredentials: () =>
+          new Promise((resolve) => {
+            resolve({ status: "SUCCESS", sessionToken: "12344SDSD" });
+          }),
+        signInWithRedirect: () => mockFunction,
       },
-    });
-    // expect(await dispatch({
-    //   type: 'loginSpace/login',
-    //   payload: {
-    //     username: 'admin@ulabsystems.net',
-    //     password: 'Qwer12#'
-    //   },
-    // }).status).toBe('SUCCESS');
+    }),
+    withOktaAuth: (x: any) => x,
+    Security: () => <div />,
+    SecureRoute: () => <div />,
+    LoginCallback: () => <div />,
+  };
+});
+
+jest.mock("react-i18next", () => ({
+  useTranslation: () => ({ t: (key: any) => key }),
+  Trans: ({ childern }: any) => childern,
+}));
+
+describe("<Login />", () => {
+  test("initial load", async () => {
+    renderWithRouter(<Login />);
+    expect(screen.getByLabelText(/formfields.password/)).toBeInTheDocument();
+    expect(screen.getByLabelText(/formfields.email/)).toBeInTheDocument();
   });
-  // afterEach(cleanup);
-  // test("initial load", () => {
-  //   const { container } = render(<Login></Login>);
-  //   expect(1);
-  // });
+  test("Should dispaly error when field is empty", async () => {
+    const { user } = renderWithRouter(<Login />);
+    await act(() => {
+      user.click(screen.getByText(/button.login/));
+    });
+    expect(
+      screen.getByText(/formfieldErrors.usernamerequired/)
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(/formfieldErrors.passwordrequired/)
+    ).toBeInTheDocument();
+  });
+  test("Should dispaly error when email is invalid format", async () => {
+    const { user } = renderWithRouter(<Login />);
+    await act(() => {
+      user.change(screen.getByLabelText("formfields.email"), {
+        target: { value: "test" },
+      });
+      user.change(screen.getByLabelText("formfields.password"), {
+        target: { value: "test" },
+      });
+      user.click(screen.getByText(/button.login/));
+    });
+    expect(
+      screen.getByText(/formfieldErrors.usernameinvalid/)
+    ).toBeInTheDocument();
+    expect(screen.queryByText(/formfieldErrors.passwordrequired/)).toBeNull();
+  });
+  test("Should able to submit form with valid username and password", async () => {
+    const { user } = renderWithRouter(<AppRoutes />, { route: "/auth/login" });
+    user.change(screen.getByLabelText("formfields.email"), {
+      target: { value: "test@ulab.com" },
+    });
+    user.change(screen.getByLabelText("formfields.password"), {
+      target: { value: "test" },
+    });
+    await act(() => {
+      user.click(screen.getByText(/button.login/));
+    });
+    // in progress need to work with valida condition
+    expect(screen.getByLabelText(/formfields.password/)).toBeInTheDocument();
+  });
+  afterEach(cleanup);
 });
