fc829c949119dfb5fc391a0ea8e4f28b658ac7b9
haonanu <haonan.wu@ulabsystems.com>
2022-11-23 13:16:47 +0800

    chore(eslinttrc.js): check import react

--- .eslinttrc.js
+++ .eslinttrc.js
@@ -1,10 +1,6 @@
 // .eslintrc.js
 module.exports = {
   extends: [require.resolve("@umijs/fabric/dist/eslint")],
-  rules: {
-    "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off",
-  },
   env: {
     browser: true,
     commonjs: true,
