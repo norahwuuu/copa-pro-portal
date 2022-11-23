f2b3cfa979fd3451b725959ee090dad1a3fded39
haonanu <haonan.wu@ulabsystems.com>
2022-11-23 11:35:36 +0800

    Revert "feat(eslinttrc): disable the rule react/jsx-runtime"
    This reverts commit ab9aca2b145e556389b868ef278d80ff537143a5.

--- .eslinttrc.js
+++ .eslinttrc.js
@@ -2,13 +2,8 @@
 module.exports = {
   extends: [require.resolve("@umijs/fabric/dist/eslint")],
   rules: {
-    // suppress errors for missing 'import React' in files
+    "react/jsx-uses-react": "off",
     "react/react-in-jsx-scope": "off",
-    // allow jsx syntax in js files (for next.js project)
-    "react/jsx-filename-extension": [
-      1,
-      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
-    ], //should add ".ts" if typescript project
   },
   env: {
     browser: true,
