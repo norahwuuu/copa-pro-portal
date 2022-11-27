c5b68ed740cc259059b9155926bb4dac61b9dc34
Jacky <houxue.liu@ulabsystems.com>
2022-11-27 17:56:50 +0800

    feat: login background png resize by screen size

---
+++ src/components/LoginBg/loginBg.style.ts
@@ -0,0 +1,227 @@
+import { Box, styled } from "@mui/system";
+export const BgLeft1 = styled(Box)(({ theme }) => ({
+  [theme.breakpoints.down(1195)]: {
+    left: "64px",
+    top: "48px",
+  },
+  [theme.breakpoints.between(1195, 1281)]: {
+    left: "64px",
+    top: "53px",
+  },
+  [theme.breakpoints.between(1281, 1367)]: {
+    left: "59px",
+    top: "42px",
+  },
+  [theme.breakpoints.between(1367, 1921)]: {
+    left: "153px",
+    top: "91px",
+  },
+  [theme.breakpoints.up(1920)]: {
+    left: "153px",
+    top: "91px",
+  },
+}));
+export const BgLeft2 = styled(Box)(({ theme }) => ({
+  [theme.breakpoints.down(1195)]: {
+    left: "284px",
+    top: "82px",
+  },
+  [theme.breakpoints.between(1195, 1281)]: {
+    left: "299px",
+    top: "94px",
+  },
+  [theme.breakpoints.between(1281, 1367)]: {
+    left: "284px",
+    top: "83px",
+  },
+  [theme.breakpoints.between(1367, 1920)]: {
+    left: "284px",
+    top: "132px",
+  },
+  [theme.breakpoints.up(1920)]: {
+    left: "497px",
+    top: "132px",
+  },
+}));
+export const BgLeft3 = styled(Box)(({ theme }) => ({
+  [theme.breakpoints.down(1195)]: {
+    left: "13px",
+    top: "96px",
+  },
+  [theme.breakpoints.between(1195, 1281)]: {
+    left: "13px",
+    top: "101px",
+  },
+  [theme.breakpoints.between(1281, 1367)]: {
+    left: "8px",
+    top: "90px",
+  },
+  [theme.breakpoints.between(1367, 1920)]: {
+    left: "8px",
+    top: "90px",
+  },
+  [theme.breakpoints.up(1920)]: {
+    left: "108px",
+    top: "213px",
+  },
+}));
+export const BgLeft4 = styled(Box)(({ theme }) => ({
+  [theme.breakpoints.down(1195)]: {
+    left: "298px",
+    top: "484px",
+  },
+  [theme.breakpoints.between(1195, 1281)]: {
+    left: "355px",
+    top: "483px",
+  },
+  [theme.breakpoints.between(1281, 1367)]: {
+    left: "350px",
+    top: "433px",
+  },
+  [theme.breakpoints.between(1367, 1920)]: {
+    left: "350px",
+    top: "433px",
+  },
+  [theme.breakpoints.up(1920)]: {
+    left: "571px",
+    top: "630px",
+  },
+}));
+export const BgLeft5 = styled(Box)(({ theme }) => ({
+  [theme.breakpoints.down(1195)]: {
+    left: "261px",
+    top: "479px",
+  },
+  [theme.breakpoints.between(1195, 1281)]: {
+    left: "318px",
+    top: "496px",
+  },
+  [theme.breakpoints.between(1281, 1367)]: {
+    left: "313px",
+    top: "446px",
+  },
+  [theme.breakpoints.between(1367, 1920)]: {
+    left: "313px",
+    top: "446px",
+  },
+  [theme.breakpoints.up(1920)]: {
+    left: "522px",
+    top: "643px",
+  },
+}));
+export const BgRight1 = styled(Box)(({ theme }) => ({
+  [theme.breakpoints.down(1195)]: {
+    left: "809px",
+    top: "46px",
+  },
+  [theme.breakpoints.between(1195, 1281)]: {
+    left: "865px",
+    top: "37px",
+  },
+  [theme.breakpoints.between(1281, 1367)]: {
+    left: "925px",
+    top: "32px",
+  },
+  [theme.breakpoints.between(1367, 1491)]: {
+    left: "925px",
+    top: "32px",
+  },
+  [theme.breakpoints.between(1491, 1631)]: {
+    left: "1025px",
+    top: "32px",
+  },
+  [theme.breakpoints.between(1631, 1920)]: {
+    left: "1251px",
+    top: "98px",
+  },
+  [theme.breakpoints.up(1920)]: {
+    left: "1251px",
+    top: "98px",
+  },
+}));
+export const BgRight2 = styled(Box)(({ theme }) => ({
+  [theme.breakpoints.down(1195)]: {
+    left: "844px",
+    top: "46px",
+  },
+  [theme.breakpoints.between(1195, 1221)]: {
+    left: "850px",
+    top: "37px",
+  },
+  [theme.breakpoints.between(1221, 1281)]: {
+    left: "900px",
+    top: "37px",
+  },
+  [theme.breakpoints.between(1281, 1491)]: {
+    left: "960px",
+    top: "32px",
+  },
+  [theme.breakpoints.between(1491, 1631)]: {
+    left: "1060px",
+    top: "32px",
+  },
+  [theme.breakpoints.between(1631, 1731)]: {
+    left: "1251px",
+    top: "98px",
+  },
+  [theme.breakpoints.between(1731, 1920)]: {
+    left: "1410px",
+    top: "76px",
+  },
+  [theme.breakpoints.up(1920)]: {
+    left: "1410px",
+    top: "76px",
+  },
+}));
+export const BgRight3 = styled(Box)(({ theme }) => ({
+  [theme.breakpoints.down(1195)]: {
+    left: "827px",
+    top: "337px",
+  },
+  [theme.breakpoints.between(1195, 1281)]: {
+    left: "883px",
+    top: "328px",
+  },
+  [theme.breakpoints.between(1281, 1441)]: {
+    left: "914px",
+    top: "332px",
+  },
+  [theme.breakpoints.between(1441, 1632)]: {
+    left: "1000px",
+    top: "332px",
+  },
+  [theme.breakpoints.between(1632, 1920)]: {
+    left: "1200px",
+    top: "332px",
+  },
+  [theme.breakpoints.up(1920)]: {
+    left: "1256px",
+    top: "470px",
+  },
+}));
+export const BgRight4 = styled(Box)(({ theme }) => ({
+  [theme.breakpoints.down(1195)]: {
+    left: "853px",
+    top: "355px",
+  },
+  [theme.breakpoints.between(1195, 1281)]: {
+    left: "909px",
+    top: "346px",
+  },
+  [theme.breakpoints.between(1281, 1441)]: {
+    left: "940px",
+    top: "350px",
+  },
+  [theme.breakpoints.between(1441, 1632)]: {
+    left: "1026px",
+    top: "350px",
+  },
+  [theme.breakpoints.between(1632, 1920)]: {
+    left: "1226px",
+    top: "350px",
+  },
+  [theme.breakpoints.up(1920)]: {
+    left: "1282px",
+    top: "488px",
+  },
+}));
--- src/components/LoginBg/loginBg.tsx
+++ src/components/LoginBg/loginBg.tsx
@@ -1,5 +1,16 @@
 import { Box } from "@mui/material";
+import { BgLeft1, BgLeft2, BgLeft3, BgLeft4, BgLeft5, BgRight1, BgRight2, BgRight3, BgRight4 } from "./loginBg.style";
+import left1 from '@/assets/images/left1.png';
+import left2 from '@/assets/images/left2.png';
+import left3 from '@/assets/images/left3.png';
+import left4 from '@/assets/images/left4.png';
+import left5 from '@/assets/images/left5.png';
+import right1 from '@/assets/images/right1.png';
+import right2 from '@/assets/images/right2.png';
+import right3 from '@/assets/images/right3.png';
+import right4 from '@/assets/images/right4.png';
 import React from 'react';
+
 
 const LoginBg = () => {
   return (
@@ -14,7 +25,36 @@ const LoginBg = () => {
         backgroundColor: "#f6f6f6",
         // backgroundImage: `url(${login1680})`,
       }}
-    ></Box>
+
+    >
+      <BgLeft1 sx={{ position: 'absolute', width: "25px", height: "32px" }}>
+        <img src={left1} />
+      </BgLeft1>
+      <BgLeft2 sx={{ position: 'absolute', width: "38px", height: "61px" }}>
+        <img src={left2} />
+      </BgLeft2>
+      <BgLeft3 sx={{ position: 'absolute', width: "343px", height: "343px" }}>
+        <img src={left3} />
+      </BgLeft3>
+      <BgLeft4 sx={{ position: 'absolute', width: "9px", height: "8px" }}>
+        <img src={left4} />
+      </BgLeft4>
+      <BgLeft5 sx={{ position: 'absolute', width: "24px", height: "37px" }}>
+        <img src={left5} />
+      </BgLeft5>
+      <BgRight1 sx={{ position: 'absolute', width: "38px", height: "52px" }}>
+        <img src={right1} />
+      </BgRight1>
+      <BgRight2 sx={{ position: 'absolute', width: "275px", height: "275px" }}>
+        <img src={right2} />
+      </BgRight2>
+      <BgRight3 sx={{ position: 'absolute', width: "14px", height: "14px" }}>
+        <img src={right3} />
+      </BgRight3>
+      <BgRight4 sx={{ position: 'absolute', width: "23px", height: "36px" }}>
+        <img src={right4} />
+      </BgRight4>
+    </Box>
   );
 };
 export default LoginBg;
