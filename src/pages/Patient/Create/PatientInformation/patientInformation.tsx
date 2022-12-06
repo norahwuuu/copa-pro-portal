0a4a11a6f6b3cbb7d980d55df814d872a185b412
songsikai <sikai.song@ulabsystems.cn>
2022-12-06 13:33:02 +0800

    feat: patientiformation

--- src/pages/Patient/Create/PatientInformation/patientInformation.tsx
+++ src/pages/Patient/Create/PatientInformation/patientInformation.tsx
@@ -1,7 +1,33 @@
+import ShadowBox from "@/pages/Components/shadowBox";
+import { Box, Grid, useTheme } from "@mui/material";
 import React, { FC } from "react";
+import Text from "@/components/Text/text";
 
 const PatientInformation: FC = () => {
-  return <>{"Patient Information"}</>;
+  const theme = useTheme();
+
+  return <Grid item xs={12} xl={12}>
+    <ShadowBox sxProp={{
+      paddingLeft: "30px",
+      height: '372px',
+      padding: '30px 0',
+      [theme.breakpoints.up("xl")]: { padding: "50px 0", height: '408px' },
+
+      flex: '1'
+    }}>
+      <Text
+        variant={"h4"}
+        color={"gray.main"}
+        sxProp={{ display: "inline-block", marginBottom: "8px" }}
+      >
+        {"Patient Information"}
+      </Text>
+      <Grid container height={"100%"} >
+        <Grid item sm={6}>1</Grid>
+        <Grid item sm={6} sx={{ borderLeft: "1px solid #CCCCCC" }}>2</Grid>
+      </Grid>
+    </ShadowBox >
+  </Grid >
 };
 
 export default PatientInformation;
