2e141423052d78219a5ff712165e92f6784b4873
songsikai <sikai.song@ulabsystems.cn>
2022-12-12 15:16:59 +0800

    fix: recoverPassword encode password into base64 format

--- .umirc.local.ts
+++ .umirc.local.ts
@@ -3,13 +3,18 @@ import { defineConfig } from "umi";
 export default defineConfig({
   proxy: {
     "/api": {
-      target: "https://apis.dev.oemaligner.com",
+      target: "https://apis.qa.oemaligner.com",
       changeOrigin: true,
       pathRewrite: { "^/api": "" },
+    },
+    "/sss": {
+      target: "https://qasec.ulabsystems.net",
+      changeOrigin: true,
+      pathRewrite: { "^/sss": "" },
     },
   },
   define: {
     BASE_URL: "/api",
-    OKTA_URL: "https://devsec.ulabsystems.net",
+    OKTA_URL: "/sss",
   },
 });
