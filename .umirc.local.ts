59652d26ec77d6a387c6adeeb1790993e984ea70
songsikai <sikai.song@ulabsystems.cn>
2022-12-12 15:18:36 +0800

    refactor: .local.ts

--- .umirc.local.ts
+++ .umirc.local.ts
@@ -3,18 +3,13 @@ import { defineConfig } from "umi";
 export default defineConfig({
   proxy: {
     "/api": {
-      target: "https://apis.qa.oemaligner.com",
+      target: "https://apis.dev.oemaligner.com",
       changeOrigin: true,
       pathRewrite: { "^/api": "" },
-    },
-    "/sss": {
-      target: "https://qasec.ulabsystems.net",
-      changeOrigin: true,
-      pathRewrite: { "^/sss": "" },
     },
   },
   define: {
     BASE_URL: "/api",
-    OKTA_URL: "/sss",
+    OKTA_URL: "https://devsec.ulabsystems.net",
   },
 });
