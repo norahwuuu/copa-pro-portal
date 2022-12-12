3f5f75f1ecc9ac67417a42d9359b6b27f70e0f2c
Jacky <houxue.liu@ulabsystems.com>
2022-12-12 11:36:10 +0800

    feat: ppc-201:ui clinical finished

--- src/pages/Patient/Create/ClinicalInformation/column.ts
+++ src/pages/Patient/Create/ClinicalInformation/column.ts
@@ -15,3 +15,17 @@ export const conditions = [
     name: "Excessive calculus",
   },
 ];
+export const monthBtns = [
+  {
+    name: "0-3",
+    label: "0-3 months",
+  },
+  {
+    name: "3-6",
+    label: "3-6 months",
+  },
+  {
+    name: "6+",
+    label: "6+ months",
+  },
+];
