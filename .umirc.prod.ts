72079940b18839578410785797e985916739f9ff
Jacky <houxue.liu@ulabsystems.com>
2022-12-20 11:34:35 +0800

    feat: client_id and issuer config in qa

--- .umirc.prod.ts
+++ .umirc.prod.ts
@@ -5,5 +5,7 @@ export default defineConfig({
     BASE_URL: "https://apis.oemaligner.com",
     OKTA_URL: "https://sec.ulabsystems.net",
     "process.env.UMI_ENV": "production",
+    CLIENT_ID: "0oa6s60ztcLzLKNhT5d7",
+    ISSUER: "https://ulab-ciam-dev.okta.com/oauth2/aus6exjzwihWqpfxl5d7",
   },
 });
--- src/declare.d.ts
+++ src/declare.d.ts
@@ -1,2 +1,4 @@
 declare let BASE_URL: string;
 declare let OKTA_URL: string;
+declare let CLIENT_ID: string;
+declare let ISSUER: string;
